e9d71e5945923140baff8e45dc23bf7542ee6c31
Vivian Allen <mrvivianallen@gmail.com>
2018-04-23 17:51:39 +0100

    depositing function of account class, tests green

--- account.py
+++ account.py
@@ -4,3 +4,7 @@ class Account(object):
 
     def __init__(self, deposit_class=Deposit):
         self.deposit_class = deposit_class
+        self.deposits = []
+
+    def deposit(self, value):
+        self.deposits.append(self.deposit_class(value))
--- test/test_account.py
+++ test/test_account.py
@@ -15,4 +15,4 @@ class AccountTestSuite(unittest.TestCase):
 
     def test_making_a_deposit(self):
         self.account.deposit(100)
-        self.assertEqual(self.deposits[0].value(), 100)
+        self.assertEqual(self.account.deposits[0].value, 100)
